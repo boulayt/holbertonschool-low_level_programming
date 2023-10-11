fe7d05d1478d85de3e52532df9feb925339a66d5
boulayt <thibault.boulay@hotmail.fr>
2023-10-11 07:49:18 -0700

    Task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of varous types
+ *
+ * Return: 0 if printf works
+ */
+
+int main(void)
+{
+    printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %dbyte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+    return (0);
+}
