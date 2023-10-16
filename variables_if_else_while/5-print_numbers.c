d19bbc6c9674a1e53cfa699d29de5c369f691bb1
boulayt <thibault.boulay@hotmail.fr>
2023-10-16 05:02:25 -0700

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Description: prints alphabet
+ * Return: 0 when done
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <10; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
