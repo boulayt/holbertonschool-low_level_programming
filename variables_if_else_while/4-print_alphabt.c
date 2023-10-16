72036c908d7b88dd36ac66e0f0703b9548d32b1b
boulayt <thibault.boulay@hotmail.fr>
2023-10-16 04:59:41 -0700

    add if in for

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Description: prints alphabet
+ * Return: 0 when done
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		if (c != 101 && c != 113)
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
