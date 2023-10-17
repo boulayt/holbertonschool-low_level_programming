2f20f4ea1f9a936c170620a0fdb03b1f91764850
boulayt <thibault.boulay@hotmail.fr>
2023-10-17 02:55:10 -0700

    swap main with alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
-/**
- * main - prints alphabet
- *
- * Description: null
- * Return: always 0
- */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char c;
+
+	for (c = 97; c <= 122; c++)
+		_putchar(c);
 	_putchar('\n');
-	return (0);
 }
--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
-void print_alphabet(void)
+/**
+ * main - prints alphabet
+ *
+ * Description: null
+ * Return: always 0
+ */
+int main(void)
 {
-	char c;
-
-	for (c = 97; c <= 122; c++)
-		_putchar(c);
+	print_alphabet();
+	return (0);
 }
