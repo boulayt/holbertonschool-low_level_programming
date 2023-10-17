37a038c897b32d225451bee7e28e53ba09e8a3da
boulayt <thibault.boulay@hotmail.fr>
2023-10-17 01:17:24 -0700

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "_putchar.c"
+
+/**
+ * main - prints "_putchar" in standard output
+ *
+ * Description: same
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
