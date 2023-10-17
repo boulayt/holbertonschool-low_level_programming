9aedfa80c2e2ed6981307366d5f07dd401b5f166
boulayt <thibault.boulay@hotmail.fr>
2023-10-17 02:27:35 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ * Description: null
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+		_putchar(c);
+}
