0a7f0122cb61044ab584b8cc780b0739b34f2dc8
boulayt <thibault.boulay@hotmail.fr>
2023-10-18 05:34:17 -0700

    add a loop for negative r

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -16,7 +16,9 @@ int add(int a, int b)
 
 	for (x = 1000000000; x >= 10; x = x / 10)
 	{
-		if ((r / x) > 0)
+		if ((r > 0) && (r / x) > 0)
+			_putchar('0' + (r / x));
+		if ((r < 0) && (r / x) < 0)
 			_putchar('0' + (r / x));
 	}
 	return((r % 10));
