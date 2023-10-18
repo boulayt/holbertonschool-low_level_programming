2f1e52d2e03ea75c7fb7b15e68d34dc822efbf93
boulayt <thibault.boulay@hotmail.fr>
2023-10-18 00:26:10 -0700

    Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_sign - computes the absolute value of an integer
+ * @n: number tested
+ *
+ * Description: same
+ * Return: the absolute value of n 
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * (-1));
+	return (0);
+}
