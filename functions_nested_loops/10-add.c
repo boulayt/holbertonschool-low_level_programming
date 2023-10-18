e327676d1f48f10325fff8889ea057266ad3cce4
boulayt <thibault.boulay@hotmail.fr>
2023-10-18 05:23:00 -0700

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: first value
+ * @b: second value
+ *
+ * Description: same
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int r, x;
+
+	r = a + b;
+
+	for (x = 1000000000; x >= 10; x = x / 10)
+	{
+		if ((r / x) > 0)
+			_putchar('0' + (r / x));
+	}
+	return((r % 10));
+}
