3b976e55ee99ff815e48ff4baafd3e622b957bdb
boulayt <thibault.boulay@hotmail.fr>
2023-10-18 08:17:52 -0700

    Tast 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print for n to 98
+ * @n: first value
+ *
+ * Description: same
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		do 
+		{
+			_putchar(n);
+		} while (n < 98);
+	}
+	if (n > 98)
+	{
+		do
+		{
+			_putchar(n);
+		} while (n > 98);
+	}
+}
