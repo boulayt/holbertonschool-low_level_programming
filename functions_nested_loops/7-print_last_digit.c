74942d46aa467b5e0f6ed43e8a2458be07d4871c
boulayt <thibault.boulay@hotmail.fr>
2023-10-18 00:38:54 -0700

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit : prints the last digit of an integer
+ * @n: number tested
+ *
+ * Description: same
+ * Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
