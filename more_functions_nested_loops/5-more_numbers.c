492aeb7cfc3a9590932b64b8f6ebde7583f0b326
boulayt <thibault.boulay@hotmail.fr>
2023-10-20 11:27:05 -0700

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,27 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers from 0 to 9
+ * more_numbers - prints numbers from 0 to 14
  *
- * Description: exclude 2 and 4
+ * Description: same
  * Return: nothing since void
  */
 void more_numbers(void)
 {
-	char i;
 
-	for (i = 48; i <= 57; i++)
+	int a, i, j;
+
+	for (a = 0; a < 10; a++)
 	{
-		if (i != 50 && i != 52)
-		_putchar(i);
-	}
+		for (i = 48; i <= 57; i++)
+		{
+			_putchar(i);
+		}
+		for (j = 0; j < 5; j++)
+		{
+			_putchar('1');
+			_putchar('0' + j);
+		}
 	_putchar('\n');
+	}
 }
