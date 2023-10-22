1ece6580fc251fef0cf5a7a2fe47b922704f44f6
boulayt <thibault.boulay@hotmail.fr>
2023-10-22 09:48:58 -0700

    rule for int_min

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit: prints the last digit of an integer
+ * print_last_digit - prints the last digit of an integer
  * @n: number tested
  *
  * Description: same
@@ -11,12 +11,16 @@ int print_last_digit(int n)
 {
 	int last;
 
+	
+	if (n == (-2147483647))
+		n = n + 10;
+
 	if (n < 0)
 		n = n * (-1);
-	if (n <= (-2147483647))
-		n = n + 100;
 
 	last = n % 10;
+	
 	_putchar('0' + last);
+
 	return (last);
 }
