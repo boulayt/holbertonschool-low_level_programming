6c31a0370ddb3f1d551cff9c08540cc01de8a556
boulayt <thibault.boulay@hotmail.fr>
2023-10-22 07:04:36 -0700

    used printf

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * fizz_buzz - prints n times char unless it multiples by 3 or 5
+ * main - prints n times char unless it multiples by 3 or 5
  *
  * Description: if (n % 3) = 0 -> prints 'Fizz'
  * if (n % 5) = 0 -> prints 'Buzz'
@@ -9,51 +9,21 @@
  * else -> prints n
  * Return: nothing since void
  */
-void fizz_buzz(void)
+int main(void)
 {
-	char i;
+	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) != 0))
-		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-		}
-		if (((i % 5) == 0) && ((i % 3) != 0))
-		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
-		if (((i % 3) == 0) && ((i % 5) == 0))
-		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		}
-		else if (i < 10)
-		{
-			_putchar(i);
-			i++;
-		}
-		else if (i < 100 && i > 10)
-		{
-			_putchar('0' + (i / 10));
-			_putchar('0' + (i % 10));
-			i++;
-		}
-		_putchar(' ');
-		}
-	_putchar('\n');
-			
-}
+		if (!(i % 3))
+			printf("Fizz");
+		if (!(i % 5))
+			printf("Buzz");
+		else if ((i % 3) && (i % 5))
+			printf("%d", i);
+		printf(" ");
+	}
+	printf ("\n");
 	
+	return (0);
+}
---
+++ more_functions_nested_loops/9-fizz_buzz_ter.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - prints n times char unless it multiples by 3 or 5
+ *
+ * Description: if (n % 3) = 0 -> prints 'Fizz'
+ * if (n % 5) = 0 -> prints 'Buzz'
+ * if both are true -> prints 'FizzBuzz'
+ * else -> prints n
+ * Return: nothing since void
+ */
+void fizz_buzz(void)
+{
+	char i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		if (((i % 5) == 0) && ((i % 3) != 0))
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		if (i < 10)
+		{
+			_putchar('0' + i);
+			i++;
+		}
+		else if (i < 100 && i > 10)
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			i++;
+		}
+		_putchar(' ');
+		}
+	_putchar('\n');
+			
+}
