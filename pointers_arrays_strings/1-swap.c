f89273c8bc094f52fdcdc4522892951838d64202
boulayt <thibault.boulay@hotmail.fr>
2023-10-23 02:03:05 -0700

    Task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of 2 int
+ * @a: input
+ * @b: second input
+ * Description: same
+ */
+void swap_int(int *a, int *b)
+{
+	int *pa = a;
+	int *pb = b;
+
+	*pa = 42;
+	*pb = 98;
+}
