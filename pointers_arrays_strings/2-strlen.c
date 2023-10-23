2c771ea497e0820b7c8d571d7742fe949f15ff88
boulayt <thibault.boulay@hotmail.fr>
2023-10-23 04:42:51 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - return length of a string
+ * @s: input
+ * Description: same
+ * Return: strlen
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
