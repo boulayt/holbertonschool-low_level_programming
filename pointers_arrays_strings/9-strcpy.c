27236c9d33a986d5242ab20e29ba5a8f023e02c3
boulayt <thibault.boulay@hotmail.fr>
2023-10-24 06:38:37 -0700

    Task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * *_strcpy - copies a string
+ * @dest: adress of return
+ * @src: string
+ * Description: same
+ * Return: value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
