ff518c53cac8adcc87bbf1d93f099b0b78a0ef73
boulayt <thibault.boulay@hotmail.fr>
2023-10-25 04:47:19 -0700

    Task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strcat: concatenates two strings
+ * @dest: adress of return
+ * @src: string
+ * Description: same
+ * Return: value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		if ((i - 1) > 0)
+			dest[i] = src[i] + src[i - 1];
+		i++;
+	}
+	return (dest);
+}
