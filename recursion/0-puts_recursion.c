c0c95c512b71d211097b72794a1e1d3cce742958
boulayt <thibault.boulay@hotmail.fr>
2023-10-30 09:28:10 -0700

    Task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -17,14 +17,7 @@ void _puts_recursion(char *s)
 
 	if (s[i] != '\0') 
 	{
-		{
-			_putchar(s[i]);
-			i++;
-		}
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
-
+		putchar(s[i]);
+		_puts_recursion(s + 1);
 	}
-
 }
