35e836377de22a131696bdb60678b55a75c919b5
boulayt <thibault.boulay@hotmail.fr>
2023-10-30 09:04:52 -0700

    Task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _puts_recursion - check the code
+ *
+ * @s: input string
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
+
+	}
+}
