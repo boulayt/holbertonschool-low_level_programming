032d78364867023a0eda5b6a6951f6eec8e00cce
boulayt <thibault.boulay@hotmail.fr>
2023-10-31 09:30:27 -0700

    Task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check the code
+ *
+ * @s: input string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	else
+		return (_strlen_recursion(s + 1) + 1);
+	
+}
