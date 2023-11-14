efcdb281eb65e74c331fc26e1e4ef979c3e43290
Thibault <thibault.boulay@hotmail.fr>
2023-11-14 01:52:37 -0800

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy a string and allocate memory for it
+ *
+ * @str: input string
+ *
+ * Return: pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	char *str_bis;
+	int i, j;
+
+	j = 0;
+	str_bis = NULL;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[j] != '\0')
+		j++;
+
+	str_bis = malloc(j);
+
+	if (str_bis == NULL)
+		return (NULL);
+
+	for (i = 0; i < j; i++)
+		str_bis[i] = str[i];
+
+	return (str_bis);
+}
