50b717d21bbc5d709a74b35ede6caa3d93a39d29
Thibault <thibault.boulay@hotmail.fr>
2023-11-14 04:46:02 -0800

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory
+ *
+ * @b: input
+ *
+ * Return: allocated space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *memory;
+
+	memory = malloc(b);
+
+	if (memory == NULL)
+		exit (98);
+
+	return (memory);
+}
