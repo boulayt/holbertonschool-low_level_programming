4a8a6ce0a2a4517ed4da31f8cfa61af6bab8778e
Thibault <thibault.boulay@hotmail.fr>
2023-11-14 01:48:59 -0800

    betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,22 +10,22 @@
  *
  * Return: pointer to the array, NULL if it fails
  */
- 
+
 char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	
+
 	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
 		return (NULL);
-	
+
 	if (size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
-		
+
 	return(array);
 }
