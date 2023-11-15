a192ac1ad943813f9a7f604e85ee592e8f9453f7
Thibault <thibault.boulay@hotmail.fr>
2023-11-15 04:01:28 -0800

    no condition for age

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,11 +21,7 @@ void print_dog(struct dog *d)
 	else
 		printf("%s\n", d->name);
 
-	printf("Age: ");
-	if (d->age == '\0')
-		printf("(nil)\n");
-	else
-		printf("%f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	printf("Owner: ");
 	if (d->owner == NULL)
