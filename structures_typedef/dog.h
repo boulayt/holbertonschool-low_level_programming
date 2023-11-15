d7a677f685786d030921629dc5ad67d654b6c690
Thibault <thibault.boulay@hotmail.fr>
2023-11-15 02:26:20 -0800

    multiple inclusion protection

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef __protect__
+#define __protect__
+
+/**
+ * struct dog - Poppy ID
+ *
+ * @name: her name
+ * @age: her age
+ * @owner: her owner
+ *
+ */
+
 struct dog
 {
-	char* name
-	float age
-	char *owner
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif
