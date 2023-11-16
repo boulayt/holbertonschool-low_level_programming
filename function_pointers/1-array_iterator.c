21fb044aa431145c362f7e7dea037afa6b46f9fe
Thibault <thibault.boulay@hotmail.fr>
2023-11-16 12:44:04 -0800

    add if (action)

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -20,6 +20,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
