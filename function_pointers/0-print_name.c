954e5d21928b3f2cef127d7f765e22625b19d663
Thibault <thibault.boulay@hotmail.fr>
2023-11-16 04:39:15 -0800

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ *
+ * @name:
+ * @f: pointer to function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef __protect__
+#define __protect__
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
