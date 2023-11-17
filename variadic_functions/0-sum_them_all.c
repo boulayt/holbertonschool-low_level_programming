df18b1a737fbb85cd66dba47c08ee93733a26648
Thibault <thibault.boulay@hotmail.fr>
2023-11-17 06:10:52 -0800

    Betty syntax

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -20,13 +20,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	result = 0;
 
-	if ( n == 0)
+	if (n == 0)
 		return (0);
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		result = result + va_arg (ap, int);
+		result = result + va_arg(ap, int);
 
 	va_end(ap);
 
