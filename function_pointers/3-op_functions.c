0fbfde7f84d1aeb0557f6dae1c61c8f0ada4e402
Thibault <thibault.boulay@hotmail.fr>
2023-11-18 05:55:18 -0800

    trying to delete error message on vi while opening 3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "calc.h"
+
+/*
+ * op_add - sums 2 integers
+ *
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/*
+ * op_sub - substracts 2 integers
+ *
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/*
+ * op_mul - multiplies 2 integers
+ *
+ * @a: first input integer
+ * @b: second input integer *
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/*
+ * op_div - divides 2 integers
+ *
+ * @a: first input integer
+ * @b: second input integer *
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/*
+ * op_div - divides 2 integers
+ *
+ * @a: first input integer
+ * @b: second input integer *
+ *
+ * Return: a / b
+ */
